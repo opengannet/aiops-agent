8bc3df78c70758c3127b8ee99b6a7664c4c2e893
kevin <27342498+winsclaw@users.noreply.github.com>
2026-07-10 18:24:08 +0800

    Remove docker-start.sh and add secondary image tag in docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -4,4 +4,5 @@ VERSION="$(git rev-parse --short HEAD)-aiops-l7fallback-$(date -u +%Y%m%d%H%M%S)
 docker build \
   --build-arg VERSION="$VERSION" \
   -t docker.fzyun.io/aiops/node-agent:latest \
+  -t push.fzyun.io/aiops/node-agent:latest \
   .
