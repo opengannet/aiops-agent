860d30ed3f8f91e86da3a7d64acbef987367ace3
kevin <kevin@apple.com.cn>
2026-07-10 10:30:29 +0000

    +

--- docker-start.sh
+++ docker-start.sh
@@ -4,6 +4,6 @@ docker run --detach --name "$(hostname)" \
   -v /sys/fs/cgroup:/host/sys/fs/cgroup:ro \
   docker.fzyun.io/aiops/node-agent:latest \
   --cgroupfs-root=/host/sys/fs/cgroup \
-  --collector-endpoint=http://172.19.66.239:8080 \
+  --collector-endpoint=https://hawk.ops.fzyun.io \
   --api-key=Bl_pq07V3MF77QhRJFunQMt9BsOuVpPs \
   --scrape-interval=30s
